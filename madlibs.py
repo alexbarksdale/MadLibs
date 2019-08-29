b3f094e151073567c5b491214e0dea16ccb1a0e2
alexbarksdale <ale.barksdale@gmail.com>
2019-08-29 14:36:12 -0700

    created input checker using regex

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,17 @@
 import random
+import re
+
+# inputChecker uses REGEX to look through the user's input and checks to see if there are only words (A-Z)
+# If it contains any special characters such as $ or @, it'll return false. '*$' means it'll check all the way through the input
+
+
+def inputChecker(userInput):
+    if re.match(r'^[a-zA-Z]*$', userInput):
+        return True
+    else:
+        return False
+
+
 # Stores the user's adjectives
 adjArray = []
 
@@ -6,18 +19,20 @@ adjArray = []
 phraseSelector = 0
 phrases = ['Enter an adjective: ', 'Enter another adjective: ',
            'Please enter another adjective: ']
-i = 0
-while i <= 2:
-    adjInput = adjArray.append(input(phrases[phraseSelector]))
+
+for userInput in phrases:
+    adjInput = input(phrases[phraseSelector])
+    if inputChecker(adjInput):
+        adjArray.append(adjInput)
     phraseSelector += 1
-    i += 1
+
 
 birdType = input('Enter a type of bird: ')
 roomLocation = input('Enter a room in a house: ')
 personName = input('Enter someone\'s name: ')
 
 # Stores the template for the story and uses f-strings (format strings)
-# 'f' in the beginning states that the string will be an f-string. It allows you to embedded Python expressions inside string contents.
+# 'f' in the beginning states that the string will be an f-string. It allows you to embedded Python expressions inside string contents
 
 
 def storyTemplate():
