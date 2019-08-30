77e9a037aca9552f156141420d12f58682f2105b
alexbarksdale <ale.barksdale@gmail.com>
2019-08-29 22:05:03 -0700

    created a dictionary of predetermined words and if the user doesn't input anything it will use the dictionary

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,7 @@
 import random
 import re
 
+print('MadLib: \nIf you do not enter a value, a random MadLib will be generated for you.')
 # inputChecker uses REGEX to look through the user's input and checks to see if there are only words (A-Z)
 # If it contains any special characters such as $ or @, it'll return false. '*$' means it'll check all the way through the input
 
@@ -13,25 +14,44 @@ def inputChecker(userInput):
         return False
 
 
+# Dictionary contains predetermined words incase the user doesn't input anything
+noInput = {
+    'adjList': ['bitter', 'disfigured', 'disloyal'],
+    'birdList': ['flamingo', 'mockingjay', 'robin'],
+    'roomList': ['basement', 'living room', 'bedroom'],
+    'nameList': ['Mike', 'Rob', 'Abby']
+}
+
+
 # Stores the user's adjectives
 adjArray = []
 
-# Asks the user THREE adjectives before looping out and asking the user the other questions
 phraseSelector = 0
 phrases = ['Enter an adjective: ', 'Enter another adjective: ',
            'Please enter another adjective: ']
 
+# Asks the user THREE adjectives before looping out and asking the user the other questions
 # While phraseSelector is less than the size of the list it will run
 # The phraseSelector will only increase if the inputChecker returns true. If it returns false it will ask the question again
 while(phraseSelector < len(phrases)):
     adjInput = input(phrases[phraseSelector])
-    if inputChecker(adjInput):
+    # Checks to see if the input is blank (false). If it's false it will get a word from the dictionary
+    if not adjInput.strip():
+        adjArray.append(random.choice(noInput.get('adjList')))
+        phraseSelector += 1
+    elif inputChecker(adjInput):
         adjArray.append(adjInput)
         phraseSelector += 1
 
 birdType = input('Enter a type of bird: ')
+if not birdType.strip():
+    birdType = random.choice(noInput.get('birdList'))
 roomLocation = input('Enter a room in a house: ')
+if not roomLocation.strip():
+    roomLocation = random.choice(noInput.get('roomList'))
 personName = input('Enter someone\'s name: ')
+if not personName.strip():
+    personName = random.choice(noInput.get('nameList'))
 
 # Stores the template for the story and uses f-strings (format strings)
 # 'f' in the beginning states that the string will be an f-string. It allows you to embedded Python expressions inside string contents
